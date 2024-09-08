9d2e7a6af4ddcea6d1a266f8484f3aa15e8c2b8a
mrpranay04 <166718872+mrpranay04@users.noreply.github.com>
2024-09-08 19:02:16 +0000

    fjyj

--- basic.cpp
+++ basic.cpp
@@ -1,44 +1,42 @@
 #include<iostream>
 using namespace std;
+int main()
+{
+    int n,m,i,j;
 
-int main() {
-    int n, m, i, j;
+    cout<<"enter the size of first array"<<endl;
+    cin>>n;
+    cout<<"enter the size of second array"<<endl;
+    cin>>m;
+    int arr1[n],arr2[m],arr3[n+m];
+    for(i=0;i<n;i++)
+    {
+        cout<<"enter the elements of the first array"<<i+1<< " : "<<endl;
+        cin>>arr1[i];
+    }
+    for(j=0;j<m;j++)
+    {
+        cout<<"enter the elements of the second array"<<j+1<< " : "<<endl;
+        cin>>arr2[j];
+    }
+    for(i=0;i<n;i++)
+     {
 
-    cout << "Enter the size of the first array: ";
-    cin >> n;
+         arr3[i]=arr1[i];
+       
+     }
+     for(j=0;j<m;j++)
+     {
+         arr3[n+j]=arr2[j];
+     }
+    cout<<"array after merging"<<endl;
 
-    cout << "Enter the size of the second array: ";
-    cin >> m;
-
-    int arr1[n], arr2[m], arr3[n + m];
-
-    // Input elements for the first array
-    for (i = 0; i < n; i++) {
-        cout << "Enter element " << i + 1 << " of the first array: ";
-        cin >> arr1[i];
+    for(i=0;i<(n+m);i++)
+    {
+        cout<<"value of an array at"<<i+1<<":"<<arr3[i]<<endl;
     }
 
-    // Input elements for the second array
-    for (j = 0; j < m; j++) {
-        cout << "Enter element " << j + 1 << " of the second array: ";
-        cin >> arr2[j];
-    }
+     return 0;
 
-    // Copy elements of the first array into the merged array
-    for (i = 0; i < n; i++) {
-        arr3[i] = arr1[i];
-    }
-
-    // Copy elements of the second array into the merged array
-    for (j = 0; j < m; j++) {
-        arr3[n + j] = arr2[j];
-    }
-
-    // Print the merged array
-    cout << "Array after merging: " << endl;
-    for (i = 0; i < (n + m); i++) {
-        cout << "Value at index " << i + 1 << ": " << arr3[i] << endl;
-    }
-
-    return 0;
 }
+
